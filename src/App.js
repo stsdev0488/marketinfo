ce76e59ba50aff5e81a84a8a48b65d1dc108b37e
stsdev0488 <sts.dev0488@gmail.com>
2020-09-14 23:44:35 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MarketInfo from './pages/MarketInfo';
+import './styles/base.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <MarketInfo />
+    </div>
+  );
+}
+
+export default App;
